e396407aa1de3046be3ab6b124da92c75a53070b
Richard <biometric.cn@gmail.com>
2013-08-14 13:37:40 +0800

    add row action

--- xadmin/static/xadmin/js/xadmin.plugin.actions.js
+++ xadmin/static/xadmin/js/xadmin.plugin.actions.js
@@ -111,7 +111,11 @@
         selectedClass: "warning"
     }
 
-    $.do_action = function(name){
+    $.do_action = function(name, pk){
+      if(pk){
+          $('#changelist-form #select-row').val(pk)
+      }else
+          $('#changelist-form #select-row').val('')
       $('#action').val(name);
       $('#changelist-form').submit();
     }
